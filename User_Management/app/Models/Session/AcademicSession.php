17979b0a2126253f5d3623d2a8e5ae286d43af42
Priyanshi Rathore <priyanshirathore57@gmail.com>
2025-09-17 17:09:34 +0530

    Integrated version1

---
+++ User_Management/app/Models/Session/AcademicSession.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Session;
+
+// the official MongoDB Eloquent base class
+use MongoDB\Laravel\Eloquent\Model as Eloquent;
+
+class AcademicSession extends Eloquent
+{
+    protected $connection = 'mongodb';     // <- use your mongodb connection from config/database.php
+    protected $collection = 'academic_sessions';    // <- name of your Mongo collection
+
+// fields 
+    protected $fillable = [
+        'serial',      // optional stored serial number
+        'name',
+        'start_date',
+        'end_date',
+        'status'
+    ];
+
+    // enable timestamps  (created_at/updated_at saved)
+    // public $timestamps = true;
+
+}
